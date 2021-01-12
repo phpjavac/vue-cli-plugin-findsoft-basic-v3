00e59cae136694ccac222bcdfa886c56cfcc9673
SunMagic <1060854705@qq.com>
2021-01-12 13:17:53 +0800

    修复mock模式

--- example/src/mock/mock_api.ts
+++ example/src/mock/mock_api.ts
@@ -1,75 +1,31 @@
-import Mock from './index';
-import {
-  getByCode, login, getStudentList, delteStudent,
-  getUserList,
-  resetPassword,
-  delteTeacher,
-  changeUserName,
-  createUser,
-} from './modules/user';
-import {
-  getClassList, editClass, delClass, insertStudent,
-} from './modules/classe';
-import { changeUserSignatures, changePasswords } from './modules/public/userInfo';
-
 export default class MockPath {
-    login = './mock/user/login';
+    login = './api/user/login';
 
-    getByCode = './mock/user/getByCode';
+    getByCode = './api/user/getByCode';
 
-    getClassList = './mock/class/queryClass'
+    getClassList = './api/class/queryClass'
 
-    editClass = './mock/class/createOrUpdateClass'
+    editClass = './api/class/createOrUpdateClass'
 
-    delClass = './mock/userManage/delClass'
+    delClass = './api/userManage/delClass'
 
-    insertStudent = './mock/classMember/insertStudent'
+    insertStudent = './api/classMember/insertStudent'
 
-    getUserList = './mock/user/queryUserList'
+    getUserList = './api/user/queryUserList'
 
-    getStudentList = './mock/classMember/listStudent'
+    getStudentList = './api/classMember/listStudent'
 
-    resetPassword = './mock/user/adminResetPassword'
+    resetPassword = './api/user/adminResetPassword'
 
-    delteTeacher = './mock/userManage/delTeacher'
+    delteTeacher = './api/userManage/delTeacher'
 
-    delteStudent = './mock/userManage/delStudent'
+    delteStudent = './api/userManage/delStudent'
 
-    changeUserName = './mock/user/changeUserInfo'
+    changeUserName = './api/user/changeUserInfo'
 
-    createUser = './mock/classMember/createClassMember'
+    createUser = './api/classMember/createClassMember'
 
-    changeUserSignatures = './mock/user/changeUserSignature'
+    changeUserSignatures = './api/user/changeUserSignature'
 
-    changePasswords = './mock/user/changePassword'
+    changePasswords = './api/user/changePassword'
 }
-
-const mockPath = new MockPath();
-// 登录模块
-Mock.mock(mockPath.login, 'post', login);
-Mock.mock(mockPath.getByCode, 'post', getByCode);
-
-// 班级模块
-Mock.mock(mockPath.getClassList, 'post', getClassList);
-Mock.mock(mockPath.editClass, 'post', editClass);
-Mock.mock(mockPath.delClass, 'post', delClass);
-Mock.mock(mockPath.insertStudent, 'post', insertStudent);
-
-// 教师列表
-Mock.mock(mockPath.getUserList, 'post', getUserList);
-// 学生列表
-Mock.mock(mockPath.getStudentList, 'post', getStudentList);
-// 重置密码
-Mock.mock(mockPath.resetPassword, 'post', resetPassword);
-// 删除教师
-Mock.mock(mockPath.delteTeacher, 'post', delteTeacher);
-// 删除学生
-Mock.mock(mockPath.delteStudent, 'post', delteStudent);
-// 编辑用户姓名
-Mock.mock(mockPath.changeUserName, 'post', changeUserName);
-// 添加用户
-Mock.mock(mockPath.createUser, 'post', createUser);
-// 更改个人信息
-Mock.mock(mockPath.changeUserSignatures, 'post', changeUserSignatures);
-// 修改密码
-Mock.mock(mockPath.changePasswords, 'post', changePasswords);
