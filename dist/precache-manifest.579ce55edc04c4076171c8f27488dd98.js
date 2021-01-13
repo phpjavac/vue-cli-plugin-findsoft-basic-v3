cf41998e5d54edd0f283dffaaa00981a00b9ecb0
CurryPaste <2771885247@qq.com>
2021-01-13 09:50:53 +0800

    不忽略dist

---
+++ dist/precache-manifest.579ce55edc04c4076171c8f27488dd98.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fda6ef2fecf66f41a66d",
+    "url": "css/Teacher.819496b7.css"
+  },
+  {
+    "revision": "79cf7d64b305cf60c1b5",
+    "url": "css/app.63ac3da8.css"
+  },
+  {
+    "revision": "2cef8065e7e7625727c5",
+    "url": "css/chunk-0679c518.198a3317.css"
+  },
+  {
+    "revision": "c5b69796eb2fa097f004",
+    "url": "css/chunk-4c885ba2.27603f4d.css"
+  },
+  {
+    "revision": "55d5385234cdf022e477",
+    "url": "css/chunk-521264ee.160f80c5.css"
+  },
+  {
+    "revision": "2651993ef73a07480ff7",
+    "url": "css/chunk-53784e4d.e61c823d.css"
+  },
+  {
+    "revision": "f958db8484fe189137ce",
+    "url": "css/chunk-6c42494b.2d47614f.css"
+  },
+  {
+    "revision": "ec2e2042425cc64d09ce",
+    "url": "css/chunk-71265fca.819092da.css"
+  },
+  {
+    "revision": "fe15533235b1c1efb73b",
+    "url": "css/chunk-770a2bb4.819092da.css"
+  },
+  {
+    "revision": "1cb1020ca7811234e146",
+    "url": "css/chunk-7f873b91.7a09a67f.css"
+  },
+  {
+    "revision": "7a2e6ad11cb7e13a2f2b",
+    "url": "css/chunk-8283e2c0.b4353482.css"
+  },
+  {
+    "revision": "bf7bcf20f69c36401242",
+    "url": "css/chunk-b76d3712.819092da.css"
+  },
+  {
+    "revision": "8e13f2304eb75a1ddc0b",
+    "url": "css/chunk-vendors.2be749e2.css"
+  },
+  {
+    "revision": "612468cc4cdb12d68dba",
+    "url": "css/home.e7b7c4e8.css"
+  },
+  {
+    "revision": "aaaa796aeb20eb6efc1e",
+    "url": "css/userlist_index.9b041fc6.css"
+  },
+  {
+    "revision": "4a7b3266f7a90b840ab3c67a39160ca7",
+    "url": "img/base-icon/iconfont.js"
+  },
+  {
+    "revision": "861c137b4677ce4cbd045df08e279874",
+    "url": "img/bg.861c137b.png"
+  },
+  {
+    "revision": "5b233ba03c5d23542fbcaba01a54846f",
+    "url": "index.html"
+  },
+  {
+    "revision": "fda6ef2fecf66f41a66d",
+    "url": "js/Teacher.ce344957.js"
+  },
+  {
+    "revision": "79cf7d64b305cf60c1b5",
+    "url": "js/app.dc11a60d.js"
+  },
+  {
+    "revision": "2cef8065e7e7625727c5",
+    "url": "js/chunk-0679c518.a2b15c99.js"
+  },
+  {
+    "revision": "960efa00840adc4ebe97",
+    "url": "js/chunk-0a33e7cd.87bb9893.js"
+  },
+  {
+    "revision": "2f979dc28ec4df848f71",
+    "url": "js/chunk-2d0dab03.a18db47d.js"
+  },
+  {
+    "revision": "2c58e05f4fa95adc2bcf",
+    "url": "js/chunk-42aba1ad.94b9f087.js"
+  },
+  {
+    "revision": "c5b69796eb2fa097f004",
+    "url": "js/chunk-4c885ba2.bc9cad0e.js"
+  },
+  {
+    "revision": "55d5385234cdf022e477",
+    "url": "js/chunk-521264ee.7e4f0253.js"
+  },
+  {
+    "revision": "2651993ef73a07480ff7",
+    "url": "js/chunk-53784e4d.10df1e5c.js"
+  },
+  {
+    "revision": "f958db8484fe189137ce",
+    "url": "js/chunk-6c42494b.d19789a1.js"
+  },
+  {
+    "revision": "d2484501deb64d48100c",
+    "url": "js/chunk-7024a8d4.65849fb7.js"
+  },
+  {
+    "revision": "ec2e2042425cc64d09ce",
+    "url": "js/chunk-71265fca.685290e6.js"
+  },
+  {
+    "revision": "fe15533235b1c1efb73b",
+    "url": "js/chunk-770a2bb4.f5aad80f.js"
+  },
+  {
+    "revision": "1cb1020ca7811234e146",
+    "url": "js/chunk-7f873b91.969b4efb.js"
+  },
+  {
+    "revision": "7a2e6ad11cb7e13a2f2b",
+    "url": "js/chunk-8283e2c0.ac015610.js"
+  },
+  {
+    "revision": "bf7bcf20f69c36401242",
+    "url": "js/chunk-b76d3712.db395aab.js"
+  },
+  {
+    "revision": "8e13f2304eb75a1ddc0b",
+    "url": "js/chunk-vendors.7f99ab24.js"
+  },
+  {
+    "revision": "612468cc4cdb12d68dba",
+    "url": "js/home.154d68d9.js"
+  },
+  {
+    "revision": "aaaa796aeb20eb6efc1e",
+    "url": "js/userlist_index.2c14e4d1.js"
+  },
+  {
+    "revision": "ab661adc3bc0ea0f465fb5a39cf78df7",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "a8b9286a47ac44c73a329cac3da1d38c",
+    "url": "static/avatar.png"
+  },
+  {
+    "revision": "9baa1f84b504bb3d8291b42190f1b244",
+    "url": "static/css/normalize.css"
+  },
+  {
+    "revision": "b5c819f50afed610fccf2746714bf413",
+    "url": "static/data/students.xls"
+  },
+  {
+    "revision": "0e9dda2e52fe5a1da7ad664f8e8558bf",
+    "url": "static/logo/ZXlogo.ico"
+  },
+  {
+    "revision": "d40859ca0603e83adf57294dc95343b7",
+    "url": "static/logo/logo.png"
+  },
+  {
+    "revision": "17163b805d960c41e3c37b18f80ed739",
+    "url": "static/logo/logoNav.png"
+  },
+  {
+    "revision": "299cb5150a13fa7fd6ab86c91045ab0d",
+    "url": "static/logo/配置LOGO前请先阅读此文档.txt"
+  }
+]);
