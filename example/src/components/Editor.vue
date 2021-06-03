71f5800ba859a1245b607b0a94ba96528ae9bdc5
yinzhenyu-su <sharkrain@outlook.com>
2021-06-03 16:13:36 +0800

    TS-948

--- example/src/components/Editor.vue
+++ example/src/components/Editor.vue
@@ -1,12 +1,25 @@
 <template lang="pug">
-div(:id="uid")
+div(:id='uid')
 </template>
 <script lang="ts">
 import {
-  defineComponent, nextTick, onMounted, reactive, watch,
+  defineComponent,
+  nextTick,
+  onMounted,
+  reactive,
+  watch,
+  onBeforeUnmount,
+  PropType,
 } from 'vue';
 import E from 'wangeditor';
 import { uniqueId } from 'lodash';
+import { ConfigType } from 'wangeditor/dist/config/index.d';
+
+function isFunction(functionToCheck: unknown) {
+  return (
+    functionToCheck && {}.toString.call(functionToCheck) === '[object Function]'
+  );
+}
 
 export default defineComponent({
   name: 'WangEditor',
@@ -14,6 +27,10 @@ export default defineComponent({
     modelValue: {
       type: String,
       default: '',
+    },
+    config: {
+      type: Object as PropType<ConfigType>,
+      default: () => ({}),
     },
   },
   emits: ['mounted', 'update:modelValue'],
@@ -23,7 +40,26 @@ export default defineComponent({
     onMounted(async () => {
       await nextTick();
       editor = new E(`#${uid}`);
-      // editor.txt.html(props.value);
+
+      editor.config.excludeMenus = ['todo', 'video', 'code'];
+      /**
+       * 配置属性
+       */
+      let htmlCache = '';
+      Object.assign(editor.config, props.config);
+      editor.config.onchange = (html: string) => {
+        htmlCache = html;
+        emit('update:modelValue', html); // v-model:value 绑定
+        // 如果配置了 onchange 回调函数，就调用该函数
+
+        if (isFunction(props.config)) {
+          (props.config.onchange as Function)(html);
+        }
+      };
+
+      /**
+       * 初始化编辑器
+       */
       editor.create();
       emit('mounted', editor);
 
@@ -33,21 +69,21 @@ export default defineComponent({
         editor.txt.html(props.modelValue);
       }
 
-      let htmlCache = '';
-      editor.config.onchange = (html: string) => {
-        htmlCache = html;
-        emit('update:modelValue', html);
-      };
-
-      // TODO: 添加配置属性
-
       // 当 prop 变化时自动更新富文本内容
-      watch(() => props.modelValue, (v: string) => {
-        if (props.modelValue !== htmlCache) {
-          // 直接赋值会导致光标位置出错，只有当父组件主动改变props时才更新富文本内容，子组件触发的更新不会导致重新赋值
-          editor.txt.html(v);
-        }
-      }, { immediate: false });
+      watch(
+        () => props.modelValue,
+        (v: string) => {
+          if (props.modelValue !== htmlCache) {
+            // 直接赋值会导致光标位置出错，只有当父组件主动改变props时才更新富文本内容，子组件触发的更新不会导致重新赋值
+            editor.txt.html(v);
+          }
+        },
+        { immediate: false },
+      );
+    });
+    // 销毁编辑器
+    onBeforeUnmount(() => {
+      if (editor) editor.destroy();
     });
     return {
       uid,
@@ -56,5 +92,4 @@ export default defineComponent({
   },
 });
 </script>
-<style lang="stylus" scoped>
-</style>
+<style lang="stylus" scoped></style>
