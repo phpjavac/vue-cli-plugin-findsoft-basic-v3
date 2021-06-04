72dbb5bf799367bb7b043271478e33d4b1db5348
CurryPaste <2771885247@qq.com>
2021-06-04 19:08:34 +0800

    💪图片上传组件增加自由尺寸

--- example/src/components/UploadImg.vue
+++ example/src/components/UploadImg.vue
@@ -11,7 +11,8 @@
     @ok="onOk",
     :maskClosable="false"
   )
-    vue-cropper(ref="cropper", id='vue-cropper' :src="choseImg", :aspectRatio="aspectRatio" v-if='cropperShow')
+    vue-cropper(v-if='`${aspectRatio}` !== "NaN" && cropperShow', ref="cropper", :src="choseImg", :aspectRatio="aspectRatio")
+    vue-cropper(v-else-if='`${aspectRatio}` === "NaN" && cropperShow', ref="cropper", :src="choseImg")
   //- 1、如果有传过来的旧图，显示旧图
   //- 2、如果没有，显示无图
   //- 3、裁切时，也是显示旧图
@@ -82,7 +83,7 @@ export default defineComponent({
       type: String,
       default: '',
     },
-    /** 裁剪比例 */
+    /** 裁剪比例 NaN表示自由尺寸 */
     aspectRatio: {
       type: Number,
       default: 16 / 9,
